ede352087cb0295ff98056ca9e331189e5e230cb
rf_104 <kingaref13@gmail.com>
2025-05-07 12:45:02 +0600

    ReduxToolKit learn and done a Todo using it

---
+++ 11_reduxToolKitTodo/src/features/todos/todoSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    todos:[{
+        id:nanoid(),
+        text: "Learn Redux Toolkit!!",
+    }]
+}
+
+export const todoSlice = createSlice({
+    name : 'todo',
+    initialState,
+    reducers:{
+        addTodo : (state,action)=>{
+            const todo  = {
+                id:nanoid(),
+                text:action.payload,
+            }
+            state.todos.push(todo);
+            
+        },
+        removeTodo:(state,action)=>{
+            const id = action.payload;
+            state.todos = state.todos.filter((todo)=> todo.id!==id);
+        },
+        updateTodo: (state, action)=>{
+            const id = action.payload;
+            state.todos = state.todos.map((todo)=> todo.id===id? todo.text=action.payload : todo)
+        },
+    }
+})
+
+
+export const {addTodo,removeTodo,updateTodo} = todoSlice.actions;
+
+export default todoSlice.reducer;
