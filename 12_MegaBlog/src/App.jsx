111503bdd86bdbfa4f085e5221dceb3f56e3938c
rf_104 <kingaref13@gmail.com>
2025-05-08 00:39:21 +0600

    setup the project initial phase

---
+++ 12_MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APWRITE_URL);
+
+  return (
+    <>
+      <h1>Sajedullah Aref</h1>
+    </>
+  )
+}
+
+export default App
