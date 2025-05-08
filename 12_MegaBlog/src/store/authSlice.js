1a7340745cd92d0e3dc44712787eb61813a6f59d
rf_104 <kingaref13@gmail.com>
2025-05-08 23:09:44 +0600

    ReduxToolKit Part done

---
+++ 12_MegaBlog/src/store/authSlice.js
@@ -0,0 +1,29 @@
+import createSlice from '@reduxjs/toolkit';
+
+const initialState = {
+    status: false,
+    userData: null,
+}
+
+const authSlice = createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        login:(state,action)=>{
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+
+
+        ///in logout we don't neeed any action payload thats why we are not expecting it 
+        logout: (state)=>{
+            state.status = false;
+            state.userData = null;
+        },
+    }
+})
+
+
+export const {login,logout} = authSlice.actions;
+
+export default authSlice.reducers;
