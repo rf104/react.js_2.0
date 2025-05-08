ac6ca90b4e4f793049d27a02c50173fd002fea14
rf_104 <kingaref13@gmail.com>
2025-05-08 22:59:51 +0600

    done with appWrite things!!

---
+++ 12_MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import conf from '../conf.js';
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthService  {
+    client = new Client();
+    account;
+    constructor(){
+        this.client.setEndpoint(conf.appwrite.appWriteUrl) // Your API Endpoint
+                    .setProject(conf.appwrite.appWriteProjectId);
+
+        this.account  = new Account(this.client);
+    }
+
+    async createAccount({email,password,name}){
+        try{
+            const user = await this.account.create(ID.unique(),email,password,name);
+            if(user){
+                //will call another method
+                this.login(email,password);
+            }
+            else return user;
+        }catch(error){
+            throw error ;
+        }
+    }
+
+    async login(email,password){
+        try{
+            return await this.account.createEmailPasswordSession(email,password);
+        }catch(error){
+            console.log("Appwrite Error Occured!! -> "+ error);
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }catch(error){
+            console.log("Appwrite Error Occured!! -> "+ error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try{
+            return await this.account.deleteSessions();
+            ///Here i am using deleteSessions to delete alll sessions where user try to interact
+            ///but there is another method called deleteSession which will delete only the session of the user
+        }catch(error){
+
+        }
+    }
+
+};
+
+const authService = new AuthService();
+
+export default authService;
---
+++ 12_MegaBlog/src/appwrite/config.js
@@ -0,0 +1,150 @@
+import conf from '../conf/conf.js';
+import { Client, ID , Databases, Storage,Query} from 'appwrite';
+
+export class Services{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client.setEndpoint(conf.appwrite.appWriteUrl) 
+                    .setProject(conf.appwrite.appWriteProjectId);
+
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            await this.databases.createDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug, // ID.unique() will create a unique id for the document here i am using slug as the id of the document
+                     // but you can use ID.unique() to create a unique id for the document
+                {
+                    title:title,
+                    content:content,
+                    featuredImage:featuredImage,
+                    userId:userId,
+                    status:status,
+                }
+            );
+        } catch (error) {
+            console.log("Appwite Error :: CreatePost Part ",error);
+        }
+    }
+
+    async updatePoost(slug,{title,content,featuredImage,status,userId}){
+        try {
+            
+            return await this.databases.updateDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+
+            )
+
+        } catch (error) {
+            console.log("Appwite Error :: UpdatePost Part ",error);
+            return false;
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            
+            await this.databases.deleteDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+            return true;
+            ///After deleting the document i want to return true as an indication
+            //here if i want to return the whole thing also but i am not doing that
+
+        } catch (error) {
+            console.log("Appwite Error :: DeletePost Part ",error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            
+            return await this.databases.getDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                
+            )
+
+        } catch (error) {
+            console.log("Appwite Error :: GetPost Part ",error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal('status', 'active')]){
+        try {
+            
+            return await this.databases.listDocuments(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                queries
+            )
+
+        } catch (error) {
+            console.log("Appwite Error :: GetPosts Part ",error);
+            return false;
+        }
+
+    }
+
+    ///upload file services!!
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwite Error :: UploadFile Part ",error);
+            return false;
+        }
+    }
+
+    ///delete file service
+
+    async deleteFIle(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appWriteBucketId,
+                fileId,
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwite Error :: DeleteFile Part ",error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appWriteBucketId,
+            fileId,
+        )
+    }
+}
+
+const services = new Services();
+
+export default services;
